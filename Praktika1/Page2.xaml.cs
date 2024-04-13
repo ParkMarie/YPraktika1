20246190ffe6934ae69a8dff5c610cd2569d5ebc
ParkMarie <park_marie@mail.ru>
2024-04-13 18:29:00 +0300

    Добавьте файлы проекта.

---
+++ Praktika1/Page2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Praktika1.BookStoreDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Praktika1.BookStoreDataSetTableAdapters;
+
+namespace Praktika1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        BooksTableAdapter Books = new BooksTableAdapter();
+        public Page2()
+        {
+            InitializeComponent();
+            BooksDataGrid.ItemsSource = Books.GetData();
+        }
+    }
+}
---
+++ Praktika1/Page4.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Praktika1.BookStoreDataSetTableAdapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Praktika1.BookStoreDataSetTableAdapters;
+
+namespace Praktika1
+{
+    /// <summary>
+    /// Логика взаимодействия для Page4.xaml
+    /// </summary>
+    public partial class Page4 : Page
+    {
+        OrderInfoTableAdapter OrderInfo = new OrderInfoTableAdapter();
+        public Page4()
+        {
+            InitializeComponent();
+            OrderInfoDataGrid.ItemsSource = OrderInfo.GetData();
+        }
+    }
+}
